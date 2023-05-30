fed4710fbbec16292aaeeaa1b2ec260932ef9208
Yash Maurya <yash.maurya537468@gmail.com>
2023-05-30 14:44:41 +0530

    completed weather app

---
+++ graphql/fetchWeatherQueries.ts
@@ -0,0 +1,93 @@
+import { gql } from "@apollo/client";
+
+export const fetchWeatherQuery = gql`
+  query MyQuery(
+    $current_weather: String
+    $daily: String = "weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max,uv_index_clear_sky_max"
+    $hourly: String = "temperature_2m,relativehumidity_2m,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,windgusts_10m,uv_index,uv_index_clear_sky"
+    $latitude: String!
+    $longitude: String!
+    $timezone: String!
+  ) {
+    myQuery(
+      current_weather: $current_weather
+      daily: $daily
+      hourly: $hourly
+      latitude: $latitude
+      longitude: $longitude
+      timezone: $timezone
+    ) {
+      elevation
+      generationtime_ms
+      latitude
+      longitude
+      timezone
+      timezone_abbreviation
+      utc_offset_seconds
+      current_weather {
+        is_day
+        temperature
+        time
+        weathercode
+        winddirection
+        windspeed
+      }
+      daily {
+        apparent_temperature_min
+        sunrise
+        sunset
+        temperature_2m_max
+        temperature_2m_min
+        time
+        uv_index_clear_sky_max
+        uv_index_max
+        weathercode
+        apparent_temperature_max
+      }
+      daily_units {
+        apparent_temperature_max
+        apparent_temperature_min
+        sunrise
+        sunset
+        temperature_2m_max
+        temperature_2m_min
+        time
+        uv_index_clear_sky_max
+        uv_index_max
+        weathercode
+      }
+      hourly {
+        apparent_temperature
+        dewpoint_2m
+        precipitation
+        precipitation_probability
+        rain
+        relativehumidity_2m
+        showers
+        snow_depth
+        snowfall
+        temperature_2m
+        time
+        uv_index
+        windgusts_10m
+        uv_index_clear_sky
+      }
+      hourly_units {
+        apparent_temperature
+        dewpoint_2m
+        precipitation
+        precipitation_probability
+        rain
+        relativehumidity_2m
+        showers
+        snow_depth
+        snowfall
+        temperature_2m
+        time
+        uv_index
+        uv_index_clear_sky
+        windgusts_10m
+      }
+    }
+  }
+`;
